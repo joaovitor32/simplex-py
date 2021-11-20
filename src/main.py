b3305224dde7e8b3bafabd136f73f54864ad17ff
João Vitor M. Lopes <joaovitormunizlopes@gmail.com>
2021-11-20 08:29:11 -0300

    Update main.py

--- src/main.py
+++ src/main.py
@@ -4,7 +4,7 @@ from Simplex.Plot import Plot
 from Tableau.SimplexTableau import SimplexTableau
 
 if __name__ == "__main__":
-    # The following linex contains the normal method
+    # The following linex that user pivoting method
     yaml_path = './src/data/simplex.yaml'
 
     plot = Plot(yaml_path)
@@ -16,7 +16,7 @@ if __name__ == "__main__":
     simplex._calculate()
     simplex.table._display_matrix()
 
-    # The following linex that user pivoting method
+    # The following linex contains the normal method
     yaml_path_tableau = './src/data/simplex-tableau.yaml'
 
     simplex_tableau = SimplexTableau(yaml_path_tableau)
